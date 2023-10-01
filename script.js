669d06707c50ee34bfacf0f256e6f22f2d465163
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-10-01 11:17:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Your code here.
 const items = document.querySelectorAll('.item')
+const container = document.querySelector('.items')
 items.forEach(el=>{
 	el.draggable = "true"
+	el.addEventListener('mousemove',()=>{
+		container.scrollLeft +=50
+	})
 })
